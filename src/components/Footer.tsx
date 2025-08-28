620a5c813d1cbe75d92a6ffe97bdb0c370d05822
Mikael <hazzler@gmail.com>
2025-08-28 11:21:47 +0200

    Update calling hours from 10:00-18:00 to 09:00-13:00 in footer and contact page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -100,7 +100,7 @@ export default function Footer() {
               <p>
                 <FaPhone /> 073-686 23 60
                 <span style={{ fontSize: '0.8rem', color: 'var(--gray-400)', marginLeft: '0.5rem' }}>
-                  10:00-18:00 Vardagar
+                  09:00-13:00 Vardagar
                 </span>
               </p>
               <p>
